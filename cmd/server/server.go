a4e6e182cf8ce0a60ef799c0c397c254cf28cb27
Todd Castilow <todd@leftslash.com>
2022-01-27 14:09:20 -0500

    comments, trailing slashes

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -10,39 +10,53 @@ import (
 
 func main() {
 
+	// setup configuration and flags
+	// allow command line flags to override file settings and edfaults
 	conf := config.NewConfig()
 	conf.Flag("env", "environment")
+	conf.Flag("config", "config file")
 	conf.Load()
 
+	// retrieve config variables
+	// note that the env variable drives selection of all other variables
 	env := conf.Get("env")
 	dbfile := conf.Get(env, "db.file")
 	addr := conf.Get(env, "net.host") + ":" + conf.Get(env, "net.port")
 	timeout := conf.Get(env, "password.reset.timeout")
 
+	// setup router, handler and auth handler
 	router := mux.NewRouter()
 	users := users.NewHandler(dbfile)
-	auth := mux.NewAuth("/login/", "/login/", users.IsValid)
+	auth := mux.NewAuth("/login", "/login", users.IsValid)
 
+	// establish logging for all handlers
 	router.Use(mux.Logger(nil))
 
+	// register User API REST handlers, all require authentication
 	router.Handle(http.MethodGet, "/api/users", auth.HandleFunc(users.GetAll))
 	router.Handle(http.MethodGet, "/api/users/{id}", auth.HandleFunc(users.Get))
 	router.Handle(http.MethodPost, "/api/users", auth.HandleFunc(users.Add))
 	router.Handle(http.MethodPut, "/api/users", auth.HandleFunc(users.Update))
 	router.Handle(http.MethodDelete, "/api/users/{id}", auth.HandleFunc(users.Delete))
 
+	// register public urls to fixed assets and login logic
 	router.Handle(http.MethodGet, "/lib/*", http.FileServer(http.Dir("static")))
 	router.Handle(http.MethodGet, "/favicon.ico", http.FileServer(http.Dir("static")))
-	router.Handle(http.MethodGet, "/login/", http.FileServer(http.Dir("static")))
+	router.Handle(http.MethodGet, "/login", http.FileServer(http.Dir("static")))
 
-	router.HandleFunc(http.MethodPost, "/forgot/", users.Forgot(timeout))
-	router.HandleFunc(http.MethodGet, "/reset/", users.SetupReset)
-	router.HandleFunc(http.MethodPost, "/reset/", users.PerformReset)
+	// register routes to handle forgotten passwords and reset logic
+	// note that users.Forgot() requires a timeout, how long a temporary password is legit
+	router.HandleFunc(http.MethodPost, "/forgot", users.Forgot(timeout))
+	router.HandleFunc(http.MethodGet, "/reset", users.SetupReset)
+	router.HandleFunc(http.MethodPost, "/reset", users.PerformReset)
 
-	router.HandleFunc(http.MethodGet, "/logout/", auth.Logout)
+	// register logout handler
+	router.HandleFunc(http.MethodGet, "/logout", auth.Logout)
 
+	// setup private / authenticated access to everything else on the server
 	router.Handle(http.MethodGet, "/*", auth.Handle(http.FileServer(http.Dir("static"))))
 	router.Handle(http.MethodPost, "/*", auth.Handle(http.FileServer(http.Dir("static"))))
 
+	// start the web server
 	router.Run(addr)
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/leftslash/users
 go 1.17
 
 require (
-	github.com/leftslash/config v0.0.0-20220117153857-591cf50a2691
-	github.com/leftslash/mux v0.0.0-20220123143544-f98fe391b0f7
-	github.com/leftslash/xerror v0.0.1
+	github.com/leftslash/config v0.0.1
+	github.com/leftslash/mux v0.0.2
+	github.com/leftslash/xerror v0.0.2
 	github.com/mattn/go-sqlite3 v1.14.10
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
 )
 
-require github.com/leftslash/xcrypto v0.0.0-20220116173505-657879ffb432 // indirect
+require github.com/leftslash/xcrypto v0.0.1 // indirect
